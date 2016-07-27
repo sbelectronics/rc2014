6f9de452260a1123242507a8be6753fb5f2b4bd6
Scott Baker <smbaker@gmail.com>
2016-07-26 23:51:21 -0700

    loadhex, savehex, and singlestep

--- supervisor/supervisor.py
+++ supervisor/supervisor.py
@@ -96,13 +96,13 @@ class Supervisor:
         self.log("not busack is received")
 
     def slow_clock(self, rate):
-        rate = (1.0/float(rate))/2.0
+        period = (1.0/float(rate))/2.0
         self.ixControl.not_gpio(0, CLKEN)
         while True:
             self.ixControl.not_gpio(0, CLKOUT)
-            time.sleep(rate)
+            time.sleep(period)
             self.ixControl.or_gpio(0, CLKOUT)
-            time.sleep(rate)
+            time.sleep(period)
 
     def normal_clock(self):
         self.ixControl.or_gpio(0, CLKEN)
@@ -149,6 +149,122 @@ class Supervisor:
         self.ixData.or_gpio(1, WR)
         self.ixData.or_gpio(1, IORQ)
 
+    def singlestep_on(self):
+        self.ixData.set_intcon(1, M1)
+        self.ixData.set_intdef(1, M1)
+        self.ixData.set_interrupt(1, M1)
+
+    def singlestep_off(self):
+        self.ixData.set_interrupt(1, 0)
+
+    def autostep(self, rate):
+        period = (1.0/float(rate))
+
+        try:
+           self.singlestep_on()
+
+           while True:
+               tnext = time.time() + period
+
+               # wait for M1 to trigger MCP23017 interrupt
+               bits = self.ixData.get_intf(1)
+               while (bits & M1) == 0:
+                   bits = self.ixData.get_intf(1)
+
+               # clear the interrupt and reset wait
+               bits=self.ixData.get_gpio(1)
+               while (bits & M1)==0:
+                  bits = self.ixData.get_gpio(1)
+
+               while (time.time()<tnext):
+                  time.sleep(0.0000001)
+        finally:
+           self.singlestep_off()
+
+class HexFile:
+    def __init__(self, addr):
+        self.addr = addr
+        self.bytes = []
+        self.bytes_per_line = 24
+
+    def write(self, val):
+        self.bytes.append(val)
+
+    def make_line(self, offset, buffer):
+        line=":%02X%04X00" % (len(buffer), offset)
+        cksum = len(buffer) + offset/256 + offset%256
+        for val in buffer:
+           line = line + "%02X" % val
+           cksum = cksum + val
+        cksum = (-(cksum % 256)) & 0xFF
+        line = line + "%02X" % cksum
+        return line
+
+    def to_str(self):
+        lines=[]
+        bytes = self.bytes[:]
+        offset = self.addr
+        while bytes:
+            buffer = bytes[:self.bytes_per_line]
+            bytes = bytes[self.bytes_per_line:]
+            lines.append(self.make_line(offset, buffer))
+            offset = offset + len(buffer)
+        lines.append(":00000001FF")
+        return "\n".join(lines)+"\n"
+
+    def read_line(self, line):
+        orig_line = line
+        if line[0]!=':' or len(line)<10:
+            print >> sys.stderr, "malformed line:", orig_line
+            return {"error": True, "is_eof": False}
+
+        line = line[1:]
+
+        rec_len = int(line[:2], 16)
+        line = line[2:]
+        addr = int(line[:4], 16)
+        line = line[4:]
+        rec = int(line[:2], 16)
+        line = line[2:]
+        if rec==1:
+            # EOF
+            return {"error": False, "is_eof": True}
+        if len(line)!=rec_len*2+2:
+            print >> sys.stderr, "malformed line:", orign_line
+            return {"error": True, "is_eof": False}
+
+        while len(self.bytes)<(rec_len + addr):
+            self.bytes.append(0)
+
+        cksum = rec_len + addr/256 + addr%256
+        for i in range(0,rec_len):
+            val = int(line[:2],16)
+            self.bytes[addr + i] = val
+            line = line[2:]
+            cksum = cksum + val
+        cksum = (-(cksum % 256)) & 0xFF
+
+        line_cksum = int(line[:2],16)
+        if line_cksum != cksum:
+            print >> sys.stderr, "checksum mismatch in line:", orig_line
+            return {"error": True, "is_eof": False}
+
+        if (self.addr==None) or (addr<self.addr):
+            self.addr = addr
+
+        return {"error": False, "is_eof": False}
+
+    def from_str(self, s):
+        self.bytes=[]
+        self.addr = None
+        for line in s.split("\n"):
+            line=line.strip()
+            status = self.read_line(line)
+            if status.get("is_eof"):
+                self.bytes = self.bytes[self.addr:]
+                return
+        print >> sys.stderr, "no eof received"
+
 def main():
     parser = OptionParser(usage="supervisor [options] command",
             description="Commands: ...")
@@ -165,6 +281,8 @@ def main():
          help="rate for slow clock", metavar="HERTZ", type="int", default=10)
     parser.add_option("-v", "--verbose", dest="verbose",
          help="verbose", action="store_true", default=False)
+    parser.add_option("-f", "--filename", dest="filename",
+         help="filename", default=None)
 
     #parser.disable_interspersed_args()
 
@@ -190,6 +308,44 @@ def main():
                     print "%04X %02X" % (i, val)
         finally:
             super.release_bus()
+
+    elif (cmd=="savehex"):
+        hexdumper = HexFile(options.addr)
+        try:
+            super.take_bus()
+            for i in range(options.addr,options.addr+options.count):
+                val = super.mem_read(i)
+                hexdumper.write(val)
+        finally:
+            super.release_bus()
+
+        if options.filename:
+            file(options.filename,"w").write(hexdumper.to_str())
+        else:
+            sys.stdout.write(hexdumper.to_str())
+
+        # a little self-test ...
+        h2 = HexFile(None)
+        h2.from_str(hexdumper.to_str())
+        print "okay?", h2.to_str() == hexdumper.to_str()
+
+
+    elif (cmd=="loadhex"):
+        hexloader = HexFile(None)
+        if options.filename:
+            hexloader.from_str(file(options.filename,"r").read())
+        else:
+            hexloader.from_str(sys.stdin.read())
+
+        try:
+            super.take_bus()
+            offset = hexloader.addr
+            for val in hexloader.bytes:
+                super.mem_write(offset, val)
+                offset = offset + 1
+        finally:
+            super.release_bus()
+
 
     elif (cmd=="peek"):
         try:
@@ -234,6 +390,53 @@ def main():
     elif (cmd=="slowclock"):
         try:
             super.slow_clock(rate=options.rate)
+        finally:
+            super.normal_clock()
+
+    elif (cmd=="singlestep"):
+        try:
+            from getch import getch
+            super.singlestep_on()
+            while True:
+                print "press `s` to step, 'q' to quit",
+                ch = getch()
+                if ch=="s":
+                    # Reading gpio will clear interrupt, but the problem is that
+                    # M1 may still be low and will immediately re-interrupt.
+                    bits=super.ixData.get_gpio(1)
+                    while (bits & M1)==0:
+                       bits = super.ixData.get_gpio(1)
+
+                    print ""
+                if ch=="q":
+                    break
+        finally:
+            super.singlestep_off()
+
+    elif (cmd=="singlestep"):
+        try:
+            from getch import getch
+            super.singlestep_on()
+            while True:
+                print "press `s` to step, 'q' to quit",
+                ch = getch()
+                if ch=="s":
+                    # Reading gpio will clear interrupt, but the problem is that
+                    # M1 may still be low and will immediately re-interrupt. So
+                    # keep reading it until we see that M1 has gone high.
+                    bits=super.ixData.get_gpio(1)
+                    while (bits & M1)==0:
+                       bits = super.ixData.get_gpio(1)
+
+                    print ""
+                if ch=="q":
+                    break
+        finally:
+            super.singlestep_off()
+
+    elif (cmd=="autostep"):
+        try:
+            super.autostep(rate=options.rate)
         finally:
             super.normal_clock()
 
--- supervisor/up.sh
+++ supervisor/up.sh
@@ -1,1 +1,1 @@
-scp ../../nixiecalc/ioexpand.py supervisor.py pi@198.0.0.233:/home/pi/
+scp ../../nixiecalc/ioexpand.py getch.py supervisor.py pi@198.0.0.233:/home/pi/
